7eb471554f13ab2b64158586349d5bd42fc2b489
Soumik Sen <imrimosen@gmail.com>
2024-01-22 11:39:26 +0530

    Initial Commit

---
+++ Messanger/MessangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessangerApp.swift
+//  Messanger
+//
+//  Created by Soumik Sen on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MessangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
